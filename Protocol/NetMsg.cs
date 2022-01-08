a427c92c1351e1b2aa3c0661f6a9d183500ab860
GAME\wb.liujin01 <2068699719@qq.com>
2022-01-08 15:35:41 +0800

    扩展网络协议体

--- Protocol/NetMsg.cs
+++ Protocol/NetMsg.cs
@@ -1,4 +1,6 @@
 ﻿using PENet;
+using System;
+using System.Collections.Generic;
 
 namespace Protocol
 {
@@ -7,6 +9,56 @@ namespace Protocol
     /// </summary>
     public class NetMsg:KCPMsg
     {
+        public CMD cmd;
+        public ErrorCode error;
+        public ReqLogin reqLogin;
+        public RspLogin rspLogin;
+    }
 
+    [Serializable]
+    public class ReqLogin
+    {
+        public string acct;
+        public string pass;
+    }
+
+    [Serializable]
+    public class RspLogin
+    {
+        public UserData userData;
+    }
+
+    [Serializable]
+    public class UserData
+    {
+        public int id;
+        public string name;
+        public int lv;
+        public int exp;
+        public int diamond;
+        public int ticket;
+        public List<HeroSelectData> heroSelectData;
+    }
+
+    [Serializable]
+    public class HeroSelectData
+    {
+        public int heroID;
+    }
+
+    // 错误码
+    public enum ErrorCode
+    {
+        None,
+        AcctIsOnline,
+    }
+
+    // 通信协议命令号
+    public enum CMD
+    {
+        None = 0,
+        // 登录
+        ReqLogin= 1,
+        RspLogin= 2,
     }
 }

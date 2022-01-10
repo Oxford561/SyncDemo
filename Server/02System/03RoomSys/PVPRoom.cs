e9542907a0578d528d59566b5722db4967cd69d7
Oxford561 <18924192170@189.cn>
2022-01-10 20:54:54 +0800

    切换状态机

--- Server/02System/03RoomSys/PVPRoom.cs
+++ Server/02System/03RoomSys/PVPRoom.cs
@@ -14,11 +14,36 @@ namespace Server
         public PVPEnum pvpEnum = PVPEnum.None;
         public ServerSession[] sessionArr;
 
+        private Dictionary<RoomStateEnum, RoomStateBase> fsm = new Dictionary<RoomStateEnum, RoomStateBase>();
+        private RoomStateEnum currentRoomStateEnum = RoomStateEnum.None;
+
         public PVPRoom(uint roomID,PVPEnum pvpEnum,ServerSession[] sessionArr)
         {
             this.roomID = roomID;
             this.pvpEnum = pvpEnum;
             this.sessionArr = sessionArr;
+
+            fsm.Add(RoomStateEnum.Confirm, new RoomStateConfirm(this));
+            fsm.Add(RoomStateEnum.Select, new RoomStateSelect(this));
+            fsm.Add(RoomStateEnum.Load, new RoomStateLoad(this));
+            fsm.Add(RoomStateEnum.Fight, new RoomStateFight(this));
+            fsm.Add(RoomStateEnum.End, new RoomStateEnd(this));
+
+            ChangeRoomState(RoomStateEnum.Confirm);
+        }
+
+        public void ChangeRoomState(RoomStateEnum targetState)
+        {
+            if (currentRoomStateEnum == targetState) return;
+            if(fsm.ContainsKey(targetState))
+            {
+                if(currentRoomStateEnum!= RoomStateEnum.None)
+                {
+                    fsm[currentRoomStateEnum].Exit();
+                }
+                fsm[targetState].Enter();
+                currentRoomStateEnum = targetState;
+            }
         }
     }
 }
